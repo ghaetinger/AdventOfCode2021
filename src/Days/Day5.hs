4c99b8f8ecfadc408b8f6f3678ba84ea0df54fd9
Guilherme Haetinger <ghaetinger@gmail.com>
2021-12-13 13:16:50 -0300

    :art: Refactored Day5

--- src/Days/Day5.hs
+++ src/Days/Day5.hs
@@ -1,41 +1,64 @@
 module Days.Day5 where
 
 import           Data.List.Split
-import Data.Map (Map)
-import qualified Data.Map as Map
-
+import           Data.Map        (Map)
+import qualified Data.Map        as Map
 
 type Pair = (Int, Int)
+
 type Vector = (Pair, Pair)
+
 type CoordMap = Map Pair Int
 
 firstQuestion :: String -> IO Int
 firstQuestion filename = do
   vectors <- readFileToVectors filename
-  let filteredVectors = filter (\((x0, y0), (x1, y1)) -> x0 == x1 || y0 == y1) vectors
+  let filteredVectors =
+        filter (\((x0, y0), (x1, y1)) -> x0 == x1 || y0 == y1) vectors
   let dict = foldl (flip countPoints) Map.empty filteredVectors
-  return (Map.foldl (\count num -> (if num >= 2 then 1 else 0) + count) 0 dict)
+  return
+    (Map.foldl
+       (\count num ->
+          (if num >= 2
+             then 1
+             else 0) +
+          count)
+       0
+       dict)
 
 secondQuestion :: String -> IO Int
 secondQuestion filename = do
   vectors <- readFileToVectors filename
-  let dict = foldl (flip countPoints) Map.empty vectors 
-  return (Map.foldl (\count num -> (if num >= 2 then 1 else 0) + count) 0 dict)
-  
+  let dict = foldl (flip countPoints) Map.empty vectors
+  return
+    (Map.foldl
+       (\count num ->
+          (if num >= 2
+             then 1
+             else 0) +
+          count)
+       0
+       dict)
+
 countPoints :: Vector -> CoordMap -> CoordMap
-countPoints vector cmap = (foldl (\cmapBuf point -> Map.insertWith (+) point 1 cmapBuf) cmap . buildLinePoints) vector
+countPoints vector cmap =
+  (foldl (\cmapBuf point -> Map.insertWith (+) point 1 cmapBuf) cmap .
+   buildLinePoints)
+    vector
 
 buildLinePoints :: Vector -> [Pair]
 buildLinePoints ((x0, y0), (x1, y1))
   | x0 /= x1 && y0 /= y1 = zip (buildNumberList x0 x1) (buildNumberList y0 y1)
-  | x0 == x1 = (\lsy -> zip (replicate (length lsy) x0) lsy) (buildNumberList y0 y1) 
-  | y0 == y1 = (\lsx -> zip lsx (replicate (length lsx) y0)) (buildNumberList x0 x1)
+  | x0 == x1 =
+    (\lsy -> zip (replicate (length lsy) x0) lsy) (buildNumberList y0 y1)
+  | y0 == y1 =
+    (\lsx -> zip lsx (replicate (length lsx) y0)) (buildNumberList x0 x1)
   | otherwise = error "This should not happen"
 
 buildNumberList :: Int -> Int -> [Int]
 buildNumberList a b
-  | a > b = reverse [b..a]
-  | otherwise = [a..b]
+  | a > b = reverse [b .. a]
+  | otherwise = [a .. b]
 
 readFileToVectors :: String -> IO [Vector]
 readFileToVectors filename = do
@@ -44,4 +67,6 @@ readFileToVectors filename = do
   return (map parseLineToVector concatLines)
 
 parseLineToVector :: String -> Vector
-parseLineToVector = (\[x,y,z,w] -> ((x, y), (z, w))) . map (read :: String -> Int) . concatMap (splitOn ",") . splitOn " -> "
+parseLineToVector =
+  (\[x, y, z, w] -> ((x, y), (z, w))) .
+  map (read :: String -> Int) . concatMap (splitOn ",") . splitOn " -> "
--- test/Days/Day5Spec.hs
+++ test/Days/Day5Spec.hs
@@ -3,8 +3,18 @@ module Days.Day5Spec where
 import           Days.Day5
 import           Test.Hspec
 
-
-exampleVectors = [((0,9), (5,9)),((8,0), (0,8)),((9,4), (3,4)),((2,2), (2,1)),((7,0), (7,4)),((6,4), (2,0)),((0,9), (2,9)),((3,4), (1,4)),((0,0), (8,8)),((5,5), (8,2))]
+exampleVectors =
+  [ ((0, 9), (5, 9))
+  , ((8, 0), (0, 8))
+  , ((9, 4), (3, 4))
+  , ((2, 2), (2, 1))
+  , ((7, 0), (7, 4))
+  , ((6, 4), (2, 0))
+  , ((0, 9), (2, 9))
+  , ((3, 4), (1, 4))
+  , ((0, 0), (8, 8))
+  , ((5, 5), (8, 2))
+  ]
 
 spec :: Spec
 spec = do
@@ -12,9 +22,9 @@ spec = do
     it "readFileToVectors" $
       readFileToVectors "./res/day5/example.txt" `shouldReturn` exampleVectors
     it "buildLinePoints" $ do
-      buildLinePoints ((2, 7), (2, 10)) `shouldBe` [(2, 7), (2, 8), (2, 9), (2, 10)]  
-      buildLinePoints ((9, 7), (7, 9)) `shouldBe` [(9, 7), (8, 8), (7, 9)]  
-
+      buildLinePoints ((2, 7), (2, 10)) `shouldBe`
+        [(2, 7), (2, 8), (2, 9), (2, 10)]
+      buildLinePoints ((9, 7), (7, 9)) `shouldBe` [(9, 7), (8, 8), (7, 9)]
   describe "First Question" $ do
     it "Example input" $ firstQuestion "./res/day5/example.txt" `shouldReturn` 5
     it "Result" $ firstQuestion "./res/day5/input.txt" `shouldReturn` 5197
