53ca44f422166367c89d372b98a20a4c2e6eaeee
Guilherme Gomes Haetinger <ghaetinger@gmail.com>
2021-12-13 15:23:42 +0000

    :sparkles: Day 6

---
+++ src/Days/Day6.hs
@@ -0,0 +1,38 @@
+module Days.Day6 where
+
+import           Data.List.Split
+
+initialFishes = [0, 0, 0, 0, 0, 0, 0]
+
+initialBreededFishes = [0, 0]
+
+firstQuestion :: String -> IO Int
+firstQuestion = runQuestion 80
+
+secondQuestion :: String -> IO Int
+secondQuestion = runQuestion 256
+
+runQuestion :: Int -> String -> IO Int
+runQuestion n filename = do
+  ls <- readFileIntoCountList filename
+  let (finalFishes, finalBreeded) =
+        foldl
+          (\(fish, breed) _ -> updateDay fish breed)
+          (ls, initialBreededFishes)
+          [1 .. n]
+  return (sum (finalFishes ++ finalBreeded))
+
+updateDay :: [Int] -> [Int] -> ([Int], [Int])
+updateDay (h:tail) (sev:eigh:_) = (tail ++ [sev + h], [eigh, h])
+updateDay _ _                   = error "This should not happen"
+
+readFileIntoCountList :: String -> IO [Int]
+readFileIntoCountList filename = do
+  contents <- readFile filename
+  let ls = (map read . splitOn ",") contents
+  return (intListToCountList ls 0)
+
+intListToCountList :: [Int] -> Int -> [Int]
+intListToCountList _ 7 = []
+intListToCountList intList n =
+  (length . filter (== n)) intList : intListToCountList intList (n + 1)
---
+++ test/Days/Day6Spec.hs
@@ -0,0 +1,22 @@
+module Days.Day6Spec where
+
+import           Days.Day6
+import           Test.Hspec
+
+spec :: Spec
+spec = do
+  describe "Auxiliary Functions" $ do
+    it "intListToCountList" $
+      intListToCountList [1, 1, 2, 3, 4, 2] 0 `shouldBe` [0, 2, 2, 1, 1, 0, 0]
+    it "runQuestion" $ runQuestion 18 "./res/day6/example.txt" `shouldReturn` 26
+  describe "First Question" $ do
+    it "Example input" $ do
+      firstQuestion "./res/day6/example.txt" `shouldReturn` 5934
+    it "Result" $ firstQuestion "./res/day6/input.txt" `shouldReturn` 375482
+  describe "Second Question" $ do
+    it "Example input" $
+      secondQuestion "./res/day6/example.txt" `shouldReturn` 26984457539
+    it "Result" $ secondQuestion "./res/day6/input.txt" `shouldReturn` 1689540415957
+
+main :: IO ()
+main = hspec spec
