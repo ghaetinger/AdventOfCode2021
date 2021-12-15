ad2575f16448be6508b8f0f4d31cb163d5270a56
Guilherme Haetinger <ghaetinger@gmail.com>
2021-12-15 15:52:36 +0000

    :sparkles: Day 10

---
+++ src/Days/Day10.hs
@@ -0,0 +1,92 @@
+{-# LANGUAGE LambdaCase #-}
+
+module Days.Day10 where
+
+import           Data.List
+import           Data.Map                       ( Map )
+import qualified Data.Map                      as Map
+import           Data.Set                       ( Set
+                                                , member
+                                                )
+import qualified Data.Set                      as Set
+
+data ParseResult = Error Char | OK | Incomplete String deriving (Show, Eq)
+
+charmap = Map.fromList [('<', '>'), ('(', ')'), ('{', '}'), ('[', ']')]
+
+endingChars = Set.fromList (Map.elems charmap)
+
+valuemap =
+  Map.fromList [('>', 25137), ('}', 1197), (']', 57), (')', 3)] :: Map Char Int
+
+complmap =
+  Map.fromList [('>', 4), ('}', 3), (']', 2), (')', 1)] :: Map Char Int
+
+firstQuestion :: String -> IO Int
+firstQuestion filename = do
+  contents <- readFile filename
+  let systems = lines contents :: [String]
+  return
+    $ ( sum
+      . map ((\c -> Map.findWithDefault 0 c valuemap) . unwrapError)
+      . filter
+          (\case
+            Error _ -> True
+            _       -> False
+          )
+      . map (`parseSubsystem` "")
+      )
+        systems
+
+secondQuestion :: String -> IO Int
+secondQuestion filename = do
+  contents <- readFile filename
+  let systems = lines contents :: [String]
+  return
+    $ ( getMiddle
+      . sort
+      . map
+          ( foldl (\acc v -> acc * 5 + v) 0
+          . map
+              (\c -> Map.findWithDefault
+                0
+                (Map.findWithDefault '_' c charmap)
+                complmap
+              )
+          . (\case
+              Incomplete s -> s
+              _            -> error "This should not happen"
+            )
+          )
+      . filter
+          (\case
+            Incomplete _ -> True
+            _            -> False
+          )
+      . map (`parseSubsystem` "")
+      )
+        systems
+
+getMiddle :: [Int] -> Int
+getMiddle ls = ls !! round (fromIntegral (length ls) / 2.0)
+
+unwrapError :: ParseResult -> Char
+unwrapError (Error c) = c
+unwrapError _         = error "This should not happen"
+
+parseSubsystem :: String -> String -> ParseResult
+parseSubsystem "" "" = OK
+parseSubsystem "" st = Incomplete st
+parseSubsystem (c : ctail) st
+  | c `Set.member` endingChars && st == ""
+  = okIfThereAreOnlyClosingFromNowOn (c : ctail)
+  | c `Set.member` endingChars && Map.lookup (head st) charmap == Just c
+  = parseSubsystem ctail (tail st)
+  | c `Set.member` endingChars
+  = Error c
+  | otherwise
+  = parseSubsystem ctail (c : st)
+
+okIfThereAreOnlyClosingFromNowOn :: String -> ParseResult
+okIfThereAreOnlyClosingFromNowOn str =
+  if all (`Set.member` endingChars) str then OK else Error (head str)
---
+++ test/Days/Day10Spec.hs
@@ -0,0 +1,30 @@
+module Days.Day10Spec where
+
+import           Days.Day10
+import           Test.Hspec
+
+spec :: Spec
+spec = do
+  describe "Auxiliary Functions" $ do
+    it "parseSubsystem" $ do
+      parseSubsystem "[(())]" "" `shouldBe` OK
+      parseSubsystem "<[(())]>" "" `shouldBe` OK
+      parseSubsystem "<[(())]>>>>>>" "" `shouldBe` OK
+      parseSubsystem "[(()))" "" `shouldBe` Error ')'
+      parseSubsystem "[}" "" `shouldBe` Error '}'
+      parseSubsystem "<<<<<<" "" `shouldBe` Incomplete "<<<<<<"
+
+  describe "First Question" $ do
+    it "Example input"
+      $              firstQuestion "./res/day10/example.txt"
+      `shouldReturn` 26397
+    it "Result" $ firstQuestion "./res/day10/input.txt" `shouldReturn` 323613
+  describe "Second Question" $ do
+    it "Example input"
+      $              secondQuestion "./res/day10/example.txt"
+      `shouldReturn` 288957
+    it "Result" $ secondQuestion "./res/day10/input.txt" `shouldReturn` 31755
+
+main :: IO ()
+main = hspec spec
+
